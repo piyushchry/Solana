97f53b26e0de27298d748eeffda38e047aed5ca4
Piyush Chaudhary <eng20cs0252.piyushchaudhary@gmail.com>
2022-12-06 15:28:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// npm init
+
 var cowsay = require("cowsay");
+var Quote = require("inspirational-quotes") 
+const quote = Quote.getRandomQuote()
 
 console.log(cowsay.say({
-    text : "Uttam CHUTTAM hai",
+    text : quote,
     e : "oO",
-    T : "U "
+    T : "U ",
 }));
